04ca2ac350ad3389ec20b8d19e7c178332e7f0a8
Daniel Lucas <dlsbdaniel@gmail.com>
2019-07-15 17:08:40 -0300

    Enabling .desktop file support on rofi launcher

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -60,6 +60,7 @@ import XMonad.Layout.ThreeColumns
 import XMonad.Layout.ResizableTile
 import XMonad.Layout.ZoomRow (zoomRow, zoomIn, zoomOut, zoomReset, ZoomMessage(ZoomFullToggle))
 import XMonad.Layout.IM (withIM, Property(Role))
+import XMonad.Layout.Tabbed
 
     -- Prompts
 import XMonad.Prompt (defaultXPConfig, XPConfig(..), XPPosition(Top), Direction1D(..))
@@ -134,7 +135,7 @@ myStartupHook = do
         spawnOnce "dropbox start"
         spawnOnce "nitrogen --restore"
         spawnOnce "compton &"
-        spawnOnce "redshift-gtk -t 3800:3800";
+        spawnOnce "redshift-gtk -l -6.466496:-37.084787 -t 3800:3800";
 
 ------------------------------------------------------------------------
 ---KEYBINDINGS
@@ -212,10 +213,10 @@ myKeys =
     -- Main Run Apps
         , ("M-<Return>", spawn myTerminal)
         , ("M-S-x", spawn "i3lock --color 475263")
-        , ("M-d", spawn "rofi -show run")
+        , ("M-d", spawn "rofi -show combi -combi-modi run,drun")
         , ("M-w", spawn "chromium")
         , ("M-n", spawn "pcmanfm")
-        , ("M-m", spawn "xfce4-terminal -e vifm")
+        , ("M-m", spawn "xfce4-terminal -e /home/daniel/.config/vifm/scripts/vifmrun")
         , ("M-<KP_Insert>", spawn "dmenu_run -fn 'UbuntuMono Nerd Font:size=10' -nb '#292d3e' -nf '#bbc5ff' -sb '#82AAFF' -sf '#292d3e' -p 'dmenu:'")
 
 
@@ -279,6 +280,7 @@ myLayoutHook = smartBorders . avoidStruts $ mouseResize $ windowArrange $ T.togg
 tall       = renamed [Replace "tall"]     $ limitWindows 12 $ spacing 6 $ ResizableTall 1 (3/100) (1/2) []
 grid       = renamed [Replace "grid"]     $ limitWindows 12 $ spacing 6 $ mkToggle (single MIRROR) $ Grid (16/10)
 threeCol   = renamed [Replace "threeCol"] $ limitWindows 3  $ ThreeCol 1 (3/100) (1/2)
+tabbedL    = renamed [Replace "tabbed"]   $ simpleTabbed
 threeRow   = renamed [Replace "threeRow"] $ limitWindows 3  $ Mirror $ mkToggle (single MIRROR) zoomRow
 oneBig     = renamed [Replace "oneBig"]   $ limitWindows 6  $ Mirror $ mkToggle (single MIRROR) $ mkToggle (single REFLECTX) $ mkToggle (single REFLECTY) $ OneBig (5/9) (8/12)
 monocle    = renamed [Replace "monocle"]  $ limitWindows 20 $ Full
